a76fc5b7a61bca4f7a0b6890eb1aa22169a692d0
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-27 20:57:58 +0500

    fix: yml

--- updated_build.sh
+++ updated_build.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
 ARCH="x86_64_linux"
 
-if [ "$1" != "free" -a "$1" != "unfree" -a "$1" != "all" ]; then
+if [ "$1" != "free" ] && [ "$1" != "unfree" ] && [ "$1" != "all" ]; then
     echo "Usage: $0 [free|unfree|all]"
     exit 1
 fi
+
 set -e
 rm -rf "dest"
-mkdir -p "dest"
-touch "dest/nixos_${ARCH}.xml"
+mkdir -p "dest/icons"
 
-combine() {
-    cp -r "$1/icons" "dest/"
+echo '<?xml version="1.0" encoding="utf-8"?>' > "dest/nixos_${ARCH}.xml"
+echo '<components version="0.14" origin="nixos">' >> "dest/nixos_${ARCH}.xml"
+
+combine_components() {
+    local source_dir="$1"
+    if [ -f "./${source_dir}/Components-x86_64-linux.xml" ]; then
+        sed '1,2d; $d' "./${source_dir}/Components-x86_64-linux.xml" >> "dest/nixos_${ARCH}.xml"
+    fi
+    
+    if [ -d "./${source_dir}/icons" ]; then
+        cp -r "./${source_dir}/icons/." "dest/icons/"
+    fi
 }
 
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<components version=\"0.14\" origin=\"nixos\">" > "dest/nixos_${ARCH}.xml"
+if [ "$1" = "all" ] || [ "$1" = "free" ]; then
+    combine_components "nixos-unstable"
+fi
 
-if [ "$1" = "all" ]; then
-	combine "nixos-unstable"
-	combine "nixos-unstable-unfree"
-else
-    combine "$1"
+if [ "$1" = "all" ] || [ "$1" = "unfree" ]; then
+    combine_components "nixos-unstable-unfree"
 fi
 
 echo "</components>" >> "dest/nixos_${ARCH}.xml"
 
 appstreamcli convert "dest/nixos_${ARCH}.xml" "dest/nixos_${ARCH}.yml"
-gzip "dest/nixos_${ARCH}.xml"
-gzip "dest/nixos_${ARCH}.yml"
+gzip -9 "dest/nixos_${ARCH}.xml"
+gzip -9 "dest/nixos_${ARCH}.yml"
+
+echo "Successfully built AppStream metadata for: $1"
